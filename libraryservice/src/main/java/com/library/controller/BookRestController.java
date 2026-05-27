d6b5c5509714cded0b7c6abcd788a62824c39c1d
aeroksingh <assign131@gmail.com>
2026-05-27 12:59:40 +0530

    Connected the H2 database

--- libraryservice/src/main/java/com/library/controller/BookRestController.java
+++ libraryservice/src/main/java/com/library/controller/BookRestController.java
@@ -5,6 +5,7 @@ import com.library.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.library.entity.Book;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookRestController {
